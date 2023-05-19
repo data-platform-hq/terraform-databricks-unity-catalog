e127dcb0acf64a81c182122e8c50732bf1fbb493
Oleh Mykolaishyn <owlleg6@gmail.com>
2023-05-19 20:33:39 +0300

    fix: added condition

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 locals {
   # This optional suffix is added to the end of resource names. 
   suffix                    = length(var.suffix) == 0 ? "" : "-${var.suffix}"
+  
   databricks_metastore_name = var.custom_databricks_metastore_name == null ? "meta-${var.project}-${var.env}-${var.location}${local.suffix}" : var.custom_databricks_metastore_name
+  
+  databricks_metastore_container_name = var.custom_databricks_metastore_container_name == null ? "meta-${var.project}-${var.env}" : var.custom_databricks_metastore_container_name
 }
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "this" {
